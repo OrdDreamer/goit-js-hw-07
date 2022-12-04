afe6ee784983ea750524d1ec381dd2fec32cc502
Roman Snitsarenko <59483945+OrdDreamer@users.noreply.github.com>
2022-12-04 19:26:18 +0200

    Render gallery items

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const getGalleryItemElement = (previewImageURL, originalImageURL, description) => {
+  return `<div class="gallery__item">
+            <a class="gallery__link" href="${originalImageURL}">
+              <img
+                class="gallery__image"
+                src="${previewImageURL}"
+                data-source="${originalImageURL}"
+                alt="${description}"
+              />
+            </a>
+          </div>`;
+};
+
+const galleryElement = document.querySelector("div.gallery");
+
+
+if (galleryElement) {
+  const galleryItemElements = galleryItems
+    .map((item) => getGalleryItemElement(item.preview, item.original, item.description))
+    .join("");
+
+    galleryElement.insertAdjacentHTML("afterbegin", galleryItemElements);
+}
