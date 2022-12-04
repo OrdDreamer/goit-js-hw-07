692b5f5feef044ba340020d6e03d0587eed6c65d
Roman Snitsarenko <59483945+OrdDreamer@users.noreply.github.com>
2022-12-04 22:13:32 +0200

    Task 1. Add opening img in modal window.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,10 @@
 import { galleryItems } from "./gallery-items.js";
 
-function getGalleryItemElement(previewImageURL, originalImageURL, description) {
+function getGalleryItemElementMarkup(
+  previewImageURL,
+  originalImageURL,
+  description
+) {
   return `<div class="gallery__item">
             <a class="gallery__link" href="${originalImageURL}">
               <img
@@ -15,10 +19,36 @@ function getGalleryItemElement(previewImageURL, originalImageURL, description) {
 
 function handleGalleryItemClick(event) {
   event.preventDefault();
+
   if (event.target.nodeName !== "IMG") {
     return;
   }
-  console.log(event.target.dataset.source);
+
+  const originalImageURL = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `<div class="modal">
+        <img src="${originalImageURL}"/>
+    </div>`,
+    {
+      closable: true,
+      className: "",
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscKeyPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
 }
 
 const galleryElement = document.querySelector("div.gallery");
@@ -26,7 +56,7 @@ const galleryElement = document.querySelector("div.gallery");
 if (galleryElement) {
   const galleryItemElements = galleryItems
     .map((item) =>
-      getGalleryItemElement(item.preview, item.original, item.description)
+      getGalleryItemElementMarkup(item.preview, item.original, item.description)
     )
     .join("");
 
