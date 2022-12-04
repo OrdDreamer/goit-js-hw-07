0aac063952280386a4d53518a2058a754ad1f358
Roman Snitsarenko <59483945+OrdDreamer@users.noreply.github.com>
2022-12-04 20:19:31 +0200

    Delegate click

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 
-const getGalleryItemElement = (previewImageURL, originalImageURL, description) => {
+function getGalleryItemElement(previewImageURL, originalImageURL, description) {
   return `<div class="gallery__item">
             <a class="gallery__link" href="${originalImageURL}">
               <img
@@ -11,15 +11,26 @@ const getGalleryItemElement = (previewImageURL, originalImageURL, description) =
               />
             </a>
           </div>`;
-};
+}
+
+function handleGalleryItemClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(event.target.dataset.source);
+}
 
 const galleryElement = document.querySelector("div.gallery");
 
-
 if (galleryElement) {
   const galleryItemElements = galleryItems
-    .map((item) => getGalleryItemElement(item.preview, item.original, item.description))
+    .map((item) =>
+      getGalleryItemElement(item.preview, item.original, item.description)
+    )
     .join("");
 
-    galleryElement.insertAdjacentHTML("afterbegin", galleryItemElements);
+  galleryElement.insertAdjacentHTML("afterbegin", galleryItemElements);
+
+  galleryElement.addEventListener("click", handleGalleryItemClick);
 }
